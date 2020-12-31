c74ddf700dd0344d5f938fb5e4d9f06bb0689bc4
TimyxiaoA <1012177593@qq.com>
2020-12-31 14:29:29 +0800

    09提交

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,28 @@
 $(function () {
 	gitUserInfo()
+
+	const layer = layui.layer
+	// 点击按钮，实现退出功能
+	$('#btnLogout').on('click', function () {
+		// 提示用户是否确认退出
+		layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function (index) {
+			//do something
+			// 1. 清空本地存储中的 token
+			localStorage.removeItem('token')
+			// 2. 重新跳转到登录页面
+			location.href = '/login.html'
+
+			// 关闭 confirm 询问框
+			layer.close(index)
+		})
+	})
 })
 
 function gitUserInfo() {
 	$.ajax({
 		method: 'GET',
-    url: '/my/userinfo',
-    // 以/my/开头的请求需要加上headers属性
+		url: '/my/userinfo',
+		// 以/my/开头的请求需要加上headers属性
 		/* headers: {
 			Authorization: localStorage.getItem('token') || ''
 		}, */
