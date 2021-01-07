5070cc3a8fc490c3b3ebab2d257c09723a2d0ae6
TimyxiaoA <1012177593@qq.com>
2021-01-07 18:28:43 +0800

    提交

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -4,13 +4,13 @@
 // 在这个函数中，可以拿到我们给Ajax提供的配置对象
 $.ajaxPrefilter(function (options) {
 	// 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
-	options.url = 'http://ajax.frontend.itheima.net' + options.url
+	options.url = 'http://api-breakingnews-web.itheima.net' + options.url;
 
 	// 统一为有权限的接口，设置 headers 请求头
 	if (options.url.indexOf('/my/') !== -1) {
 		options.headers = {
 			Authorization: localStorage.getItem('token') || ''
-		}
+		};
 	}
 
 	//  不论成功还是失败，最终都会调用 complete 回调函数
@@ -22,9 +22,9 @@ $.ajaxPrefilter(function (options) {
 
 		if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
 			// 1. 强制清空 token
-			localStorage.removeItem('token')
+			localStorage.removeItem('token');
 			// 2. 强制跳转到登录页面
-			window.top.location.href = '/login.html' //window.top顶层
+			window.top.location.href = '/login.html'; //window.top顶层
 		}
-	}
-})
+	};
+});
